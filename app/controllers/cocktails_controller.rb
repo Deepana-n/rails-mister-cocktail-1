ef811eb485f39b651008b3dfe8af2c4d650d212e
florestan lefevre <florestanlefevre@gmail.com>
2017-05-19 00:07:34 +0200

    with photos

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,46 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show]
+
+  def index
+    @cocktails = Cocktail.all
+    url = 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic'
+    cocktails_serialized = open(url).read
+    cocktails = JSON.parse(cocktails_serialized)
+    @pics = []
+
+    cocktails["drinks"].each do |cocktail|
+      if cocktail["strDrinkThumb"]
+        @pics << cocktail["strDrinkThumb"]
+      end
+      # url de pic => @pics[rand(@pics.size)]
+    end
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,59 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-# Ingredient.create(name: "lemon")
-# Ingredient.create(name: "ice")
-# Ingredient.create(name: "mint leaves")
-
 require 'json'
 require 'open-uri'
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
 
 url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients_serialized = open(url).read
 ingredients = JSON.parse(ingredients_serialized)
 
-# puts "#{ingredient["name"]} - #{ingredient["bio"]}"
 ingredients["drinks"].each do |ingredient|
   Ingredient.create(name: ingredient["strIngredient1"])
+
 end
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic'
+cocktails_serialized = open(url).read
+cocktails = JSON.parse(cocktails_serialized)
+
+cocktails["drinks"].each do |cocktail|
+  Cocktail.create(name: cocktail["strDrink"])
+end
+
+
+
+
+
+# dose1 = Dose.create(description: "5cl de gin", ingredient_id: 1, cocktail_id: 1)
+# dose2 = Dose.create(description: "15cl de vodka", ingredient_id: 2, cocktail_id: 2)
+# dose3 = Dose.create(description: "25cl de rhum", ingredient_id: 3, cocktail_id: 3)
+
+# get id en string
+# url = 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic'
+# drinks_serialized = open(url).read
+# drinks = JSON.parse(drinks_serialized)
+# ids = []
+
+# # stocker chaque id dans array
+# drinks["drinks"].each do |cocktail|
+#   ids << cocktail["idDrink"].to_i
+# end
+
+#parser avec un lien qui change en fonction id
+# 50.times do
+# id = ids[rand(ids.length)]
+
+# url = "http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{id}"
+# cocktails_serialized = open(url).read
+# cocktails = JSON.parse(cocktails_serialized)
+
+# p cocktails["drinks"]
+# p cocktails["drinks"].size
+
+# cocktails["drinks"].each do |cocktail|
+#   a = Dose.new(description: cocktail["strInstructions"], ingredient_id: rand(270), cocktail_id: rand(250))
+#   p a
+#   a.save
+# end
